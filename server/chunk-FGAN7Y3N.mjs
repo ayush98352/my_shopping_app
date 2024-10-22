42edaab9cc9f4d318250b5e32f9a6ca9556b092e
ayush98352 <ayushagrawalgaya@gmail.com>
2024-10-23 04:09:43 +0530

    Auto-generated commit

--- server/chunk-FGAN7Y3N.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{g as r}from"./chunk-Q6QYHI2G.mjs";var s=(()=>{class t{constructor(){this.data=null,this.productDetails={}}setData(a){this.data=a}getData(){return this.data}setProductDetails(a){this.productDetails=a}getProductDetails(){return this.productDetails}static{this.\u0275fac=function(e){return new(e||t)}}static{this.\u0275prov=r({token:t,factory:t.\u0275fac,providedIn:"root"})}}return t})();export{s as a};
--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6D5TA2ZD.mjs";import"./chunk-SLWMELS5.mjs";import"./chunk-5EY626D2.mjs";import"./chunk-FGAN7Y3N.mjs";import"./chunk-BF5KGVNB.mjs";import"./chunk-Q6QYHI2G.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-22A7CG7D.mjs";import"./chunk-SLWMELS5.mjs";import"./chunk-5EY626D2.mjs";import"./chunk-HYFUS7RT.mjs";import"./chunk-Q6QYHI2G.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
