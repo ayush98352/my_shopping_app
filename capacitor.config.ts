564cce25cbd5c218c061b3b2c661841080a26bd3
ayush98352 <ayushagrawalgaya@gmail.com>
2024-11-30 03:30:34 +0530

    ayush-test-xcode

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ayush.shopping.app',
+  appName: 'zenndo',
+  webDir: 'dist/my-app/browser',
+  bundledWebRuntime: false,
+};
+
+export default config;
